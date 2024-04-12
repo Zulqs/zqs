c2a663831fb2651214c043477f999a43e230da6b
Zulqs <108636905+Zulqs@users.noreply.github.com>
2024-04-13 05:31:21 +0700

    Navbar (#1)
    * Refactor navbar component to add sticky behavior on scroll
    
    * Add next-themes package and implement dark mode toggle in navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,45 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import {    
+        FaLightbulb,
+        FaRegLightbulb    
+} from 'react-icons/fa';
 
-const navbar = () => {
+const Navbar = () => {
+    const { resolvedTheme, theme, setTheme } = useTheme();
+    const [isSticky, setIsSticky] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            if (offset > 0) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
-        <div className='container flex flex-row justify-between border-y-4 border-blck'>
-            <p className='text-2xl py-4'>zqs / landing</p>
-            <div className='flex flex-row'>
-                <p className='text-2xl py-4'>Personal Website</p>
-                <div className='mx-4 border-l-4 border-blck'></div>
-                <button className='text-2xl py-4'>light</button>
+        <nav className={`container ${isSticky ? 'sticky -top-1 z-50 translate-y-3 transition-transform duration-700 ease-in-out transform' : '-translate-y-0 transition-transform duration-200 ease-out transform'}`}>
+            <div className='flex flex-row justify-between border-y-4 border-blck bg-[#f5f5f5]'>
+                <p className='text-2xl py-4'>zqs / landing</p>
+                <div className='flex flex-row'>
+                    <p className='text-2xl py-4'>Personal Website</p>
+                    <div className='mx-4 border-l-4 border-blck'></div>
+                    <button 
+                    onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+                    className='text-2xl py-4'>
+                        {theme === 'light' ? <FaLightbulb /> : <FaRegLightbulb />}
+                    </button>
+                </div>
             </div>
-        </div>
+        </nav>
     )
 }
 
-export default navbar
+export default Navbar
